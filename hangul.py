08a85813dce95c37c4e3ffdee2cf1e3844653335
takaokakegawa <takao@umich.edu>
2023-08-17 11:23:34 +0800

    html to markdown feature added.

--- hangul.py
+++ hangul.py
@@ -16,6 +16,7 @@ from location_detection import detected_potential_countries
 from report_type import detect_report_type
 from keyword_detection import generate_keywords
 from langcode_to_name import get_lang_name
+from html_to_markdown import get_markdown
 
 tika.initVM()
 nlp = spacy.load('en_core_web_md')
@@ -244,5 +245,6 @@ async def detect(file: UploadFile, kw_num: int):
         'locations': locations,
         'disasters': disasters,
         'full_content': cleaned_content,
-        'keywords': generate_keywords(doc_summary, kw_num)
+        'keywords': generate_keywords(doc_summary, kw_num),
+        'markdown': get_markdown(metadata_of_pdfs[0]['xml_content'])
     }
