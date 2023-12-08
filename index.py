65f4dce896c93c23185119128cdc275b477d0b31
Sidra Effendi <sidraeffendi@gmail.com>
2023-12-07 19:44:21 -0500

    added hangul v2 endpoint to index file

--- index.py
+++ index.py
@@ -3,14 +3,17 @@ from fastapi.middleware.cors import CORSMiddleware
 from typing import Annotated
 from chetah_v1 import search
 from hangul import detect
+from hangul import detect_second_version
 
 from models import ChetahModel
 
 d4g = FastAPI()
 
 BASE_PATH = '/api/v1/products'
+BASE_PATH_SECOND_VERSION = '/api/v2/products'
 CHETAH_PATH = f'{BASE_PATH}/chetah'
 HANGUL_PATH = f'{BASE_PATH}/hangul'
+HANGUL_SECOND_VERSION_PATH = f'{BASE_PATH_SECOND_VERSION}/hangul'
 
 origins = ["*"]
 
@@ -33,3 +36,8 @@ async def chetah(body: ChetahModel):
 async def hangul(kw_num: Annotated[int, Form()],
                  file: Annotated[UploadFile, File(description="File uploaded to the Hangul system")]):
     return await detect(file, kw_num)
+
+@d4g.post(HANGUL_SECOND_VERSION_PATH)
+async def hangul(kw_num: Annotated[int, Form()],
+                 file: Annotated[UploadFile, File(description="File uploaded to the Hangul system")]):
+    return await detect_second_version(file, kw_num)
