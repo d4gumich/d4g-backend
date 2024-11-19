54f628256b9238d1f0ddeae11cbc5112aad9e787
R-Paskvan <rpaskvan@umich.edu>
2024-11-19 15:48:10 -0500

    WIP Pipeline progress, function add

--- chetah_library/NavigateReports.py
+++ chetah_library/NavigateReports.py
@@ -1,11 +1,20 @@
 from pathlib import Path
 
-print("New")
-p = Path(".")
-p = p.cwd()
-print(p)
-p = p/'chetah_library'
-print(p)
+def retrieve_report_paths(root_folder):
+    # This function will return a list of all file paths of pdf reports
+    # matching the conditions of being a PDF and located within
+    # a "clean_eng_docs" sub-directory
 
-# With a built path, let's see if we can read in the text file
-with p.open() as f: f.readlines()
+    # Convert the string path, to a path object
+    root_folder = Path(root_folder)
+
+    # Retrieve all Report PDF's from 'clean_eng_docs' directories
+    files = list(root_folder.glob("**/clean_eng_docs/*.pdf"))
+
+    # We'll return as path objects, converting to a csv can wait.
+    # files = [str(file) for file in files]
+
+    return files
+
+
+paths = retrieve_report_paths("D:/Chetah_data_2021-20241103T230242Z-001/Chetah_data_2021")
