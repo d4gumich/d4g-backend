31533fbc33baaa18637f73c0a60d2a15b84666be
R-Paskvan <rpaskvan@umich.edu>
2025-02-10 15:44:39 -0500

    Flask WIP Changes

--- chetah_v2.py
+++ chetah_v2.py
@@ -1,1 +1,12 @@
 # this file is the main BM25F algorithm, for user searches
+import pandas as pd
+from configparser import SafeConfigParser
+import pathlib
+
+# load the inverted index and table
+directory_path = pathlib.Path(__file__).parent.resolve()
+configuration = SafeConfigParser()
+configuration.read(f'{directory_path}/configuration.ini')
+
+inv_index = configuration.get('chetah','inv_path')
+print(inv_index)
