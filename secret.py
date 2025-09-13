32c0bf6d7490781cb789ee0dbf4ca419ee4d8e28
XabUG7 <urruchua.xabier@gmail.com>
2025-09-13 17:12:35 +0400

    adding owl with full capabilities

--- secret.py
+++ secret.py
@@ -13,5 +13,7 @@ def my_keys():
     # YOU CAN GET YOUR API KEY HERE:
     # https://aistudio.google.com/prompts/new_chat
     key_dict["Google_API_key"] = "ADD HERE you API CODE"
+    key_dict["OWL_Google_API_key"] = "ADD HERE you API CODE" #"ADD HERE you API CODE"
+    key_dict["postgreSQL_pass"] = "ADD HERE you DB CODE" #"ADD HERE you DB access CODE"
     
     return key_dict
